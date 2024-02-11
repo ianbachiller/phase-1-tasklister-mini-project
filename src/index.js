b0163e95990a6d41943df80e01bd1ee8ab297517
Ian Bachiller <ianbachise@gmail.com>
2024-02-11 12:37:29 -0500

    complete mini to do list project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector("form")
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    newP(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function newP(toDoEnteredStoredHere){
+  let newUl = document.createElement('ul')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDoAndAddToCompletedList)
+  btn.textContent = ' 💖'
+  newUl.textContent= `${toDoEnteredStoredHere} `
+  newUl.appendChild(btn)
+  document.querySelector('#tasks').appendChild(newUl) 
+}
+function deleteToDoAndAddToCompletedList(e){
+  let taskItem = e.target.parentNode
+  taskItem.remove();
+
+  let newUlForCompletedToDo = document.createElement('ul')
+  // newUlForCompletedToDo.textContent = `${toDoEnteredStoredHere}: ✔️`
+
+  document.querySelector('#tasks_completed').appendChild(taskItem)
+}
